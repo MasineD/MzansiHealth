73bdba3cff0b2deb5b9a4764b9580aead3f1424b
MasineD <masinedonald@gmail.com>
2026-06-16 16:20:21 +0200

    frontendFeature(host database):This adds the code to use of a database hosted on neon

--- backend/config/database.js
+++ backend/config/database.js
@@ -5,11 +5,13 @@ import dotenv from "dotenv";    //To allow loading environment variables from a 
 dotenv.config();
 
 const pool = new Pool({
-    host: process.env.DB_HOST,       //Database host, typically 'localhost' for local development
-    port: process.env.DB_PORT,       //Database port, default is 5432 for PostgreSQL
-    user: process.env.DB_USER,       //Database user
-    password: process.env.DB_PASSWORD, //Database password
-    database: process.env.DB_NAME    //Database name
+    connectionString: process.env.DATABASE_URL, //Connection string for the database, typically in the format: 'postgresql://user:password@host:port/database'
+    ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false
+    // host: process.env.DB_HOST,       //Database host, typically 'localhost' for local development
+    // port: process.env.DB_PORT,       //Database port, default is 5432 for PostgreSQL
+    // user: process.env.DB_USER,       //Database user
+    // password: process.env.DB_PASSWORD, //Database password
+    // database: process.env.DB_NAME    //Database name
 });
 
 pool.on("connect", (client) => {      //Event listener for the 'connect' event, which is emitted when a new client is successfully connected to the database.
