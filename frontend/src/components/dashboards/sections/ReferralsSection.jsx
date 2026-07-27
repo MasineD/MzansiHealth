bbb4472494893519719b55d17aebe2e755a0d0dc
Masine Donald <masinedonald@gmail.com>
2026-07-27 21:11:01 +0200

    Configure axios for API requests
    Set axios default base URL and enable withCredentials for session management.

--- frontend/src/components/dashboards/sections/ReferralsSection.jsx
+++ frontend/src/components/dashboards/sections/ReferralsSection.jsx
@@ -4,6 +4,10 @@ import axios from 'axios';
 import { jsPDF } from 'jspdf';
 import { FaUserMd, FaUser, FaUsers, FaCalendarAlt, FaPrescriptionBottle, FaClipboardList, FaPlus, FaHeartbeat, FaRunning, FaTint, FaSignOutAlt, FaMedkit, FaFileMedicalAlt, FaTasks,FaExchangeAlt,FaStar,FaComments,FaClock,FaHospital,FaShieldAlt,FaPlusCircle,FaPaperPlane,FaBell,FaCheck } from 'react-icons/fa';
 
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL; // Target the backend server
+// const API_URL = import.meta.env.VITE_API_BASE_URL;
+axios.defaults.withCredentials = true; // This will allow axios to send cookies with requests, which is necessary for session management
+
 const ReferralsSection = ({ user }) => {
   const [referrals, setReferrals] = useState([]);
   const [loading, setLoading] = useState(true);
